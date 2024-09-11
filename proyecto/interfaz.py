3b48b93a7cbd4531421f1f77a7bc2bb2adf1a2ea
DcabezasE <125401866+DcabezasE@users.noreply.github.com>
2024-09-11 09:47:14 -0500

    interfaz.py

--- proyecto/interfaz.py
+++ proyecto/interfaz.py
@@ -1,5 +1,5 @@
 from inventario import Inventario
-from producto import Producto
+from inventario import Producto
 #se importa messagebox de tkinter para que abra una ventana emergente para mostrar informacion o un error de validación
 from tkinter import messagebox
 from tkinter import *
@@ -11,6 +11,7 @@ class GUI:
         self.root = root
         self.inventario = Inventario()  # Instanciar el inventario
         self.create_widgets()
+        self.lista_venta= []
 
     def create_widgets(self):
         self.tituloframe = Frame(self.root)
@@ -27,23 +28,26 @@ class GUI:
         self.botonSalir.grid(row=0, column=3, padx=10)
 
     def registrar_entrada(self):
+        for widget in self.root.winfo_children():
+            widget.grid_forget()
+        self.create_widgets()
         self.entradaframe = Frame(self.root, bg="#afc7b9", bd=3, relief="ridge")
         self.entradaframe.grid(row=1, column=0, padx=10, pady=10)
-        self.entradaframe.config(width="450", height="200")
+        self.entradaframe.config(width="300", height="120")
         self.entradaframe.grid_propagate(False)
 
-        Label(self.entradaframe, text="Nombre del Producto:", fg="#998b82", bg="#afc7b9", font=(15)).grid(row=0, column=0)
+        Label(self.entradaframe, text="Nombre del Producto:", fg="#20A859", bg="#afc7b9", font=(15)).grid(row=0, column=0)
         self.cuadroNombre = Entry(self.entradaframe)
         self.cuadroNombre.grid(row=0, column=1)
-        Label(self.entradaframe, text="Precio del Producto:", fg="#998b82", bg="#afc7b9", font=(15)).grid(row=1, column=0)
+        Label(self.entradaframe, text="Precio del Producto:", fg="#20A859", bg="#afc7b9", font=(15)).grid(row=1, column=0)
         self.cuadroPrecio = Entry(self.entradaframe)
         self.cuadroPrecio.grid(row=1, column=1)
-        Label(self.entradaframe, text="Cantidad a ingresar:", fg="#998b82", bg="#afc7b9", font=(15)).grid(row=2, column=0)
+        Label(self.entradaframe, text="Cantidad a ingresar:", fg="#20A859", bg="#afc7b9", font=(15)).grid(row=2, column=0)
         self.cuadroCantidad = Entry(self.entradaframe)
         self.cuadroCantidad.grid(row=2, column=1)
 
-        Button(self.entradaframe, text="Volver", command=self.volver).grid(row=3, column=0, padx=10)
-        Button(self.entradaframe, text="Enviar", command=self.enviar_entrada).grid(row=3, column=1, padx=10)
+        Button(self.entradaframe, text="Volver", command=self.volver).grid(row=3, column=0)
+        Button(self.entradaframe, text="Enviar", command=self.enviar_entrada).grid(row=3, column=1)
 
     def enviar_entrada(self):
         nombre = self.cuadroNombre.get()
@@ -59,24 +63,76 @@ class GUI:
         producto = Producto(nombre, precio, cantidad, tiempo)
         self.inventario.registrar_entrada(producto, cantidad)
         messagebox.showinfo("Registro de entrada", f"{producto.nombre} x{cantidad} - {producto.tiempo}")
-        self.volver()
 
 
     def registrar_salida(self):
+        for widget in self.root.winfo_children():
+            widget.grid_forget()
+        self.create_widgets()
         self.entradaframe = Frame(self.root, bg="#fca89d", bd=3, relief="ridge")
         self.entradaframe.grid(row=1, column=0, padx=10, pady=10)
         self.entradaframe.config(width="450", height="150")
         self.entradaframe.grid_propagate(False)
 
-        Label(self.entradaframe, text="Nombre del Producto:", fg="#998b82", bg="#fca89d", font=(15)).grid(row=0, column=0)
+        Label(self.entradaframe, text="Nombre del Producto:", fg="#9DF1FC", bg="#fca89d", font=(15)).grid(row=0, column=0)
         self.cuadroNombreSalida = Entry(self.entradaframe)
         self.cuadroNombreSalida.grid(row=0, column=1)
-        Label(self.entradaframe, text="Cantidad a registrar:", fg="#998b82", bg="#fca89d", font=(15)).grid(row=1, column=0)
+        Label(self.entradaframe, text="Cantidad a registrar:", fg="#9DF1FC", bg="#fca89d", font=(15)).grid(row=1, column=0)
         self.cuadroCantidadSalida = Entry(self.entradaframe)
         self.cuadroCantidadSalida.grid(row=1, column=1)
 
-        Button(self.entradaframe, text="Volver", command=self.volver).grid(row=2, column=0, padx=10)
-        Button(self.entradaframe, text="Enviar", command=self.enviar_salida).grid(row=2, column=1, padx=10)
+        Button(self.entradaframe, text="Volver", command=self.volver).grid(row=2, column=0)
+        Button(self.entradaframe, text="Añadir al carrito", command=self.carrito).grid(row=2, column=1)
+        Button(self.entradaframe, text="Pagar", command=self.enviar_salida).grid(row=3, column=0)
+    
+    def carrito(self):
+        self.inventario.cargar_datos()
+        nombre = self.cuadroNombreSalida.get()
+        try:
+            cantidad = int(self.cuadroCantidadSalida.get())
+        except ValueError:
+            messagebox.showerror("Error", "Cantidad debe ser un número válido")
+            return
+        
+        for p in self.inventario.productos:
+            if p.nombre == nombre:
+                self.lista_venta.append([nombre, cantidad, p.precio])
+                print(self.lista_venta)
+                self.inventario.productos.clear()
+            else:
+                messagebox.showerror("Error", "Producto no encontrado")
+                self.inventario.productos.clear()
+
+        
+    def resumen_de_venta(self):
+        for widget in self.root.winfo_children():
+            widget.grid_forget()
+        self.create_widgets()
+        self.entradaframe = Frame(self.root, bg="#fca89d", bd=3, relief="ridge")
+        self.entradaframe.grid(row=1, column=0, padx=10, pady=10)
+        self.entradaframe.config(width="450", height="150")
+        self.entradaframe.grid_propagate(False)
+
+        nombre= Entry(self.entradaframe, width=10)
+        nombre.grid(row=0, column=0)
+        nombre.insert(0, 'Nombre')
+        cantidad = Entry(self.entradaframe, width=10)
+        cantidad.grid(row=0, column=1)
+        cantidad.insert(0, 'Cantidad')
+        precio = Entry(self.entradaframe, width=10)
+        precio.grid(row=0, column=2)
+        precio.insert(0, 'A pagar')
+
+        for i,p in enumerate(self.lista_venta):
+            n = Entry(self.entradaframe, width=10)
+            n.grid(row=i+1, column=0)
+            n.insert(0, f"{p[0]}")
+            c = Entry(self.entradaframe, width=10)
+            c.grid(row=i+1, column=1)
+            c.insert(0, f"x {p[1]}")
+
+
+
 
     def enviar_salida(self):
         nombre = self.cuadroNombreSalida.get()
@@ -91,19 +147,27 @@ class GUI:
             self.inventario.registrar_salida(producto, cantidad)
         else:
             messagebox.showerror("Error", "Producto no encontrado")
-        self.volver()
 
     def mostrar_inventario(self):
+        for widget in self.root.winfo_children():
+            widget.grid_forget()
+        self.create_widgets()
+        self.inventario.cargar_datos()
         inventario_info = ""
         for p in self.inventario.productos:
             inventario_info += f"Nombre {p.nombre}: cantidad: {p.cantidad} x ${p.precio} {p.tiempo}\n"
         if not inventario_info:
             inventario_info = "El inventario está vacío."
         messagebox.showinfo("Inventario", inventario_info)
+        
     def mostrar_inventario2(self):
+        for widget in self.root.winfo_children():
+            widget.grid_forget()
+        self.create_widgets()
+        self.inventario.cargar_datos()
         self.entradaframe = Frame(self.root, bg="#fca89d", bd=3, relief="ridge")
         self.entradaframe.grid(row=1, column=0, padx=10, pady=10)
-        #self.entradaframe.config(width="450", height="150")
+        self.entradaframe.config(width="450", height="150")
         self.entradaframe.grid_propagate(False)
 
         nombre= Entry(self.entradaframe, width=10)
@@ -132,7 +196,7 @@ class GUI:
             t.grid(row=i+1, column=3)
             t.insert(0, f"{p.tiempo}")
         Button(self.entradaframe, text="Volver", command=self.volver).grid(row=i+3, column=0, padx=10)
-
+        self.inventario.productos.clear()
 
     def salida(self):
         self.root.destroy()
@@ -149,4 +213,5 @@ if __name__ == "__main__":
     raiz.config(bg="#ffe1c9", bd=20)
     app = GUI(raiz)
     raiz.mainloop()
+
 
