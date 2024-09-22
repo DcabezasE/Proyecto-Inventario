843c7026f00702ca2a8b461117f63d74d27d1ac5
DcabezasE <125401866+DcabezasE@users.noreply.github.com>
2024-09-22 16:40:37 -0500

    Delete proyecto directory

--- proyecto/producto.py
+++
@@ -1,7 +0,0 @@
-class Producto:
-    def __init__(self, nombre: str, precio: float, cantidad: int, tiempo: str):
-        self.nombre = nombre
-        self.precio = precio
-        self.cantidad = cantidad
-        self.tiempo = tiempo
-        
